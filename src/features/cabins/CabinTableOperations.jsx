ecc67585f8a257c979360f390ef2797f199093a9
zZedx <Kartikajmera890@gmail.com>
2023-10-29 15:58:03 +0530

    sort cabins

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,12 +1,32 @@
-import TableOperations from '../../ui/TableOperations'
-import Filter from '../../ui/Filter'
+import TableOperations from "../../ui/TableOperations";
+import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 
 const CabinTableOperations = () => {
   return (
-    <TableOperations> 
-        <Filter filterField={"filter"} options={[{value : 'all' , label : "All"} , {value : 'no-discount' , label : "No discount"} , {value : 'with-discount' , label : "With Discount"}]}/>
-    </TableOperations>
-  )
-}
+    <TableOperations>
+      <Filter
+        filterField={"filter"}
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "No discount" },
+          { value: "with-discount", label: "With Discount" },
+        ]}
+        defaultActive="all"
+      />
 
-export default CabinTableOperations
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Name (A-Z)" },
+          { value: "name-desc", label: "Name (Z-A)" },
+          { value: "regularPrice-asc", label: "Price (low-high)" },
+          { value: "regularPrice-desc", label: "Price (high-low)" },
+          { value: "maxCapacity-asc", label: "Capacity (low-high)" },
+          { value: "maxCapacity-desc", label: "Capacity (high-low)" },
+        ]}
+      />
+    </TableOperations>
+  );
+};
+
+export default CabinTableOperations;
