5a185b53ed7f563e43521b50329308b732c9e952
zZedx <Kartikajmera890@gmail.com>
2023-10-30 22:52:35 +0530

    filter bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 const useBookings = () => {
+  const [searchParams] = useSearchParams();
+  const filterValue = searchParams.get("status") || "all";
+
   const {
     isLoading,
     data: bookings,
     isError,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings" , filterValue],
+    queryFn: () => getBookings(filterValue),
   });
 
   return { isLoading, bookings, isError };
