a2f03408ac0b2260417164bdff2f804ee9df51a5
zZedx <Kartikajmera890@gmail.com>
2023-11-05 12:08:49 +0530

    header

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import useUser from "./useUser";
+import { useNavigate } from "react-router-dom";
 
 const StyledUserAvatar = styled.div`
   display: flex;
@@ -7,15 +9,39 @@ const StyledUserAvatar = styled.div`
   font-weight: 500;
   font-size: 1.4rem;
   color: var(--color-grey-600);
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+
+  &:hover{
+    background-color: var(--color-grey-100);
+
+    & span{
+      text-decoration: underline;
+    }
+  }
 `;
 
 const Avatar = styled.img`
   display: block;
-  width: 4rem;
-  width: 3.6rem;
+  width: 3rem;
   aspect-ratio: 1;
   object-fit: cover;
   object-position: center;
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
 `;
+
+const UserAvatar = () => {
+  const { user } = useUser()
+  const navigate = useNavigate()
+
+  return (
+    <StyledUserAvatar onClick={()=> navigate("/account")}>
+      <Avatar src={user.profilePic} alt="" />
+      <span>{user.name}</span>
+      </StyledUserAvatar>
+  )
+}
+
+export default UserAvatar
