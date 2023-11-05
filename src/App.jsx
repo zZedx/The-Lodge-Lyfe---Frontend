2e8876a62d71761faa22155bd7c86733e7c9b6e7
zZedx <Kartikajmera890@gmail.com>
2023-11-05 20:21:17 +0530

    toast color change

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ export default function App() {
             fontSize: '16px',
             maxWidth: "500px",
             padding: "16px 24px",
-            backgroundColor: 'var(--color-grey-0)',
+            backgroundColor: 'var(--color-grey-100)',
             color: 'var(--color-grey-700)'
           }
 
