61fda0d02f69ea27a9ecfbf82523e1cfc969cfbd
zZedx <Kartikajmera890@gmail.com>
2023-11-06 22:19:36 +0530

    removed console.log

--- src/features/check-in-out/TodayActivity.jsx
+++ src/features/check-in-out/TodayActivity.jsx
@@ -40,7 +40,6 @@ const NoActivity = styled.p`
 
 function TodayActivity() {
   const { activities, isLoading, isError } = useTodayActivity();
-  console.log(activities)
   return (
     <StyledToday>
       <Row type="horizontal">
